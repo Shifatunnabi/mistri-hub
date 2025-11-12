e52dc56214742c729fe00a82874e1cf1c6a0a292
shifatXplore <shifatunnabishifat@gmail.com>
2025-11-13 00:05:22 +0600

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p className="text-9xl">🧑‍🔧🛠️</p>
+      <h1 className="text-9xl font-bold text-blue-500">MistriHub</h1>
+      <br />
+      <p className="text-2xl font-xl"> 🛠️🛠️  Under Development  🛠️🛠️ </p>
+    </div>
+  );
+}
